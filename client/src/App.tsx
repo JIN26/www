10005c93631341bf98f1711af5ca4c4aeb541f81
ruranjo <rubens.rangel.job@gmail.com>
2023-11-26 22:40:33 -0400

    my changing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,19 @@
 import { Menu, Navbar } from './components/common';
-import { Home, Product, Products, User, Users } from './pages';
+import {Home, Product, Products, User, Users } from './pages';
 import './styles/app.scss'
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-//import Users from "./pages/users/Users";
-//import Products from "./pages/products/Products";
-// import Navbar from "./components/navbar/Navbar";
-// import Footer from "./components/footer/Footer";
-// import Menu from "./components/menu/Menu";
-// import Login from "./pages/login/Login";
-// import "./styles/global.scss";
-// import User from "./pages/user/User";
-// import Product from "./pages/product/Product";
-// import { QueryClient, QueryClientProvider, } from "@tanstack/react-query";
 
-
-//const queryClient = new QueryClient();
-
-function App() {
+function App(){
   const Layout = () => {
     return (
       <div className="main">
        <Navbar />  
         <div className="container">
+          
           <div className="menuContainer">
-            
             <Menu />
           </div>
+
           <div className="contentContainer">
             {/*<QueryClientProvider client={queryClient}>
               
@@ -67,7 +55,6 @@ function App() {
         },
       ],
     },
-    
   ]);
 
   return <RouterProvider router={router} />;
