4fdc9b94d006bb2e28325d3acd4dbb85fbd042fb
JIN26 <Angelmantilla26@gmail.com>
2023-12-09 13:55:53 -0500

    add some => comparacion de fechas para el calendario

---
+++ client/src/build/util/07-some.js
@@ -0,0 +1,35 @@
+import { areIntervalsOverlapping } from 'date-fns'
+
+const dates = [
+  {
+    startDate: new Date(2021, 1, 1, 10),
+    endDate: new Date(2021, 1, 1, 11),
+    title: "Cita de trabajo",
+  },
+  {
+    startDate: new Date(2021, 1, 1, 15),
+    endDate: new Date(2021, 1, 1, 15, 30),
+    title: "Cita con mi jefe",
+  },
+  {
+    startDate: new Date(2021, 1, 1, 20),
+    endDate: new Date(2021, 1, 1, 21),
+    title: "Cena",
+  },
+];
+
+const newAppointment = {
+  startDate: new Date(2021, 1, 1, 8),
+  endDate: new Date(2021, 1, 1, 9, 30),
+};
+
+const isOverlap = (newDate) => {
+    return dates.some(date => {
+        return areIntervalsOverlapping(
+            {start: date.startDate, end: date.endDate},
+            {start: newDate.startDate, end: newDate.endDate},
+        )
+    })
+}
+
+console.log('isOverlap', isOverlap(newAppointment));
